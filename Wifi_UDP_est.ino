eb3a6903efcfd491447502e41d47fe5bb70b3547
Ayush Dwivedi <general.quantumparticle@gmail.com>
2025-08-21 21:33:01 +0530

    Update Wifi_UDP_est.ino

--- Wifi_UDP_est.ino
+++ Wifi_UDP_est.ino
@@ -2,12 +2,10 @@
 #include <WiFiUdp.h>
 #include "HX711.h"
 
-
 const char* ssid     = "GSS-2";
 const char* password = "esp32www";
-
 const char* laptopIP = "192.168.137.1";
-const int port = 9500;  
+const int port = 9500;
 
 WiFiUDP udp;
 
@@ -15,17 +13,17 @@ WiFiUDP udp;
 #define SCK 19
 HX711 scale;
 
-float calibration_factor = 8.47;  
+float calibration_factor = -8.47;
 
 void setup() {
   Serial.begin(115200);
 
   scale.begin(DT, SCK);
   scale.set_scale(calibration_factor);
-  scale.tare();  
+  scale.tare();
 
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to Wi-Fi"); //Dbt
+  Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -38,27 +36,21 @@ void setup() {
   Serial.println(laptopIP);
 }
 
-void loop()
-{
-  static unsigned long last_time = 0;
-  unsigned long now = millis();
+void loop() {
+  // Read as soon as HX711 has a new sample
+  if (scale.is_ready()) {
+    float wei = scale.get_units(1);   // single read
+    float Thrust = wei / 100;
+    if (fabs(Thrust) < 0.05) Thrust = 0;
 
-  if (now - last_time >= 12) {  // every 12.5 ms
-    last_time = now;
+    char buffer[16];
+    snprintf(buffer, sizeof(buffer), "%.2f", Thrust);
 
-    if (scale.is_ready()) {
-      float wei = scale.get_units(1);  
-      float Thrust = wei / 100;
-      if (abs(Thrust) < 0.05) Thrust = 0;
+    udp.beginPacket(laptopIP, port);
+    udp.write((uint8_t*)buffer, strlen(buffer));  // Cast + give length
+    udp.endPacket();
 
-      char buffer[32];
-      snprintf(buffer, sizeof(buffer), "%.2f", Thrust);
-      udp.beginPacket(laptopIP, port);
-      udp.print(buffer);
-      udp.endPacket();
-
-      Serial.println(Thrust);
-    }
+    Serial.println(Thrust);
   }
 }
 
